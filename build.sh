47d159f702a264c11aee4bd09dc215f33fdfb07e
Julian <julian@strawberryfoundations.xyz>
2024-04-24 21:12:26 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target=x86_64-unknown-linux-gnu
-upx target/x86_64-unknown-linux-gnu/release/initd
-
-cp target/x86_64-unknown-linux-gnu/release/initd ~/Projekte/initramfs/src/initd
-cp -r openinit.d ~/Projekte/initramfs/src/etc/
