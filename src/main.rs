9f95d0600fbaa6886ed0be8b2c61661a34a4a8fe
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-11 15:21:27 +0200

    added panic & shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::core::init::OpenInit;
 use crate::core::err::InitError;
 use crate::daemon::system::OpenDaemon;
-use crate::sys::reboot;
+use crate::sys::sysrq;
 use crate::cli::args::{Args, Command};
 use crate::cli::help;
 use crate::colors::{BOLD, C_RESET, RED};
@@ -15,16 +15,20 @@ mod cli;
 
 fn main() {
     let args = Args::collect();
-    
+
     let mut openinit = OpenInit::new();
     let opendaemon = OpenDaemon::new();
-    
+
     match args.command {
         Command::Help => help::help(),
         Command::Reboot => {
-            reboot::sys_reboot();
+            sysrq::sys_reboot();
         },
-        Command::None => {
+        Command::Shutdown => {
+            sysrq::sys_shutdown();
+        },
+        Command::Panic => panic!("!!! Explicit kernel panic"),
+        _ => {
             openinit.start(opendaemon).unwrap_or_else(|err| {
                 eprintln!("{RED}{BOLD}{err}{C_RESET}");
 
