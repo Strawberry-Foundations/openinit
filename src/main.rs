54f0b6400a8bfda9f183ed854168e494fa48a779
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-11 14:33:24 +0200

    changes to init system

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
-use crate::core::init::{init, OpenInit};
-use crate::daemon::system::OpenDaemon;
+use crate::core::init::OpenInit;
 use crate::core::err::InitError;
-
+use crate::daemon::system::OpenDaemon;
+use crate::sys::reboot;
+use crate::cli::args::{Args, Command};
+use crate::cli::help;
 use crate::colors::{BOLD, C_RESET, RED};
 
 mod colors;
 mod core;
 mod daemon;
 mod util;
+mod sys;
+mod cli;
 
 fn main() {
+    let args = Args::collect();
+    
     let mut openinit = OpenInit::new();
     let opendaemon = OpenDaemon::new();
+    
+    match args.command {
+        Command::Help => help::help(),
+        Command::Reboot => {
+            reboot::sys_reboot();
+        },
+        Command::None => {
+            openinit.start(opendaemon).unwrap_or_else(|err| {
+                eprintln!("{RED}{BOLD}{err}{C_RESET}");
 
-    openinit.start(opendaemon).unwrap_or_else(|err| {
-        eprintln!("{RED}{BOLD}{err}{C_RESET}");
-
-        if err != InitError::AlreadyRunning {
-            openinit.stop();
-        } else {
-            std::process::exit(1);
-        }
-    });
-
-
-    match init() {
-        Ok(..) => { },
-        Err(err) => {
-            eprintln!("{RED}{BOLD}Error: {err}{C_RESET}");
-            std::process::exit(1);
+                if err != InitError::AlreadyRunning {
+                    openinit.stop();
+                } else {
+                    std::process::exit(1);
+                }
+            });
         }
     }
 }
