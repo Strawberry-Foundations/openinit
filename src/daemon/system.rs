aea6dfb5b87ed46dd2186480cd693e888047dcea
Juliandev02 <julian@strawberryfoundations.org>
2025-02-19 21:13:44 +0100

    refactor: clean up imports and improve code formatting

--- src/daemon/system.rs
+++ src/daemon/system.rs
@@ -1,29 +1,25 @@
+use crate::colors::{BOLD, C_RESET, GRAY, GREEN, RESET};
+use crate::core::err::InitError;
+use crate::core::log::{log_fail, log_info, log_startup, log_success};
+use crate::core::target::{PostTarget, Target};
+use crate::daemon::service::OpenService;
+use crate::util::{delete_last_line, get_os_name};
+use crate::CLEANUP_SCREEN;
+
 use std::io::{stdout, Write};
 use std::process::{Command, Stdio};
 use std::sync::mpsc;
 use std::thread::spawn;
-
 use eyre::Result;
-use crate::CLEANUP_SCREEN;
-
-use crate::core::err::InitError;
-use crate::core::target::{PostTarget, Target};
-use crate::core::log::{log_fail, log_info, log_startup, log_success};
-use crate::daemon::service::OpenService;
-use crate::util::{delete_last_line, get_os_name};
-use crate::colors::{BOLD, C_RESET, GRAY, GREEN, RESET};
-
 
 #[derive(Default)]
 pub struct OpenDaemon {
-    pub services: Vec<OpenService>
+    pub services: Vec<OpenService>,
 }
 
 impl OpenDaemon {
     pub fn new() -> Self {
-        Self {
-            services: vec![]
-        }
+        Self { services: vec![] }
     }
 
     pub fn configure(&mut self) -> Result<(), InitError> {
@@ -31,7 +27,7 @@ impl OpenDaemon {
 
         let services = match std::fs::read_dir(service_folder) {
             Ok(res) => res,
-            Err(_) => return Err(InitError::ServiceDirectoryNotFound)
+            Err(_) => return Err(InitError::ServiceDirectoryNotFound),
         };
 
         // todo: remove unwrap
@@ -39,7 +35,8 @@ impl OpenDaemon {
             let entry = service.unwrap();
             let service_path = entry.path();
 
-            if service_path.is_file() && (service_path.extension().unwrap_or_default() == "yaml" || service_path.extension().unwrap_or_default() == "yml") {
+            if service_path.is_file() && (service_path.extension().unwrap_or_default() == "yaml" || service_path.extension().unwrap_or_default() == "yml")
+            {
                 let service_content = std::fs::read_to_string(&service_path).unwrap();
                 let data: OpenService = serde_yaml::from_str(&service_content).unwrap();
 
@@ -55,16 +52,21 @@ impl OpenDaemon {
             let target = Target::new(service);
 
             if target.post == PostTarget::Shell {
-                continue
+                continue;
             }
-            
+
             log_startup(service);
 
             if let Some(info) = &service.service.info {
                 log_info(service, info)
             }
 
-            let command: Vec<String> = service.service.command.split_whitespace().map(String::from).collect();
+            let command: Vec<String> = service
+                .service
+                .command
+                .split_whitespace()
+                .map(String::from)
+                .collect();
 
             if let Some((cmd, args)) = command.split_first() {
                 if target.post == PostTarget::Loop {
@@ -74,35 +76,36 @@ impl OpenDaemon {
                     let args_cloned = args.to_owned().clone();
                     let service_cloned = service.clone();
 
-                    let handle = std::thread::Builder::new().name("service_thread".to_string()).spawn(move || {
-                        match Command::new(cmd)
-                            .args(args_cloned)
-                            .stdout(Stdio::null())
-                            .stderr(Stdio::piped())
-                            .spawn() {
-                            Ok(res) => {
-                                tx.send(true).unwrap();
-                                res
-                            }
-                            Err(_) => {
-                                tx.send(false).unwrap();
-
-                                Command::new("false").spawn().unwrap()
-                            }
-                        }
-                    });
-
-                    match rx.recv().unwrap() {
-                        true => {
-                            match handle {
-                                Ok(_) => {
-                                    delete_last_line();
-                                    log_success(service);
+                    let handle = std::thread::Builder::new()
+                        .name("service_thread".to_string())
+                        .spawn(move || {
+                            match Command::new(cmd)
+                                .args(args_cloned)
+                                .stdout(Stdio::null())
+                                .stderr(Stdio::piped())
+                                .spawn()
+                            {
+                                Ok(res) => {
+                                    tx.send(true).unwrap();
+                                    res
                                 }
                                 Err(_) => {
-                                    delete_last_line();
-                                    log_fail(service);
+                                    tx.send(false).unwrap();
+
+                                    Command::new("false").spawn().unwrap()
                                 }
+                            }
+                        });
+
+                    match rx.recv().unwrap() {
+                        true => match handle {
+                            Ok(_) => {
+                                delete_last_line();
+                                log_success(service);
+                            }
+                            Err(_) => {
+                                delete_last_line();
+                                log_fail(service);
                             }
                         },
                         false => {
@@ -110,14 +113,14 @@ impl OpenDaemon {
                             log_fail(&service_cloned);
                         }
                     };
-                }
-                else {
+                } else {
                     let child = match Command::new(cmd)
                         .args(args)
                         .stdout(Stdio::null())
                         .stderr(Stdio::piped())
-                        .status() {
-                        Ok(res) => { res }
+                        .status()
+                    {
+                        Ok(res) => res,
                         Err(_) => {
                             delete_last_line();
                             log_fail(service);
@@ -129,7 +132,7 @@ impl OpenDaemon {
                     if child.code().unwrap() == 1 {
                         delete_last_line();
                         log_fail(service);
-                        
+
                         continue;
                     }
 
@@ -142,10 +145,14 @@ impl OpenDaemon {
             }
         }
 
-        let service = &self.services.iter().find(|service| {
-            let target = Target::new(service);
-            target.post == PostTarget::Shell
-        }).unwrap();
+        let service = &self
+            .services
+            .iter()
+            .find(|service| {
+                let target = Target::new(service);
+                target.post == PostTarget::Shell
+            })
+            .unwrap();
 
         if let Some(info) = &service.service.info {
             log_info(service, info)
@@ -218,7 +225,7 @@ impl OpenDaemon {
             Ok(_) => {
                 delete_last_line();
                 log_success(service);
-            },
+            }
             Err(_) => {
                 log_fail(service);
                 std::process::exit(1);
@@ -242,7 +249,6 @@ impl OpenDaemon {
                 dbg!("Err");
             }
         } */
-
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 #![allow(dead_code)]
 
-use crate::core::init::OpenInit;
-use crate::core::err::InitError;
-use crate::daemon::system::OpenDaemon;
-use crate::sys::sysrq;
 use crate::cli::args::{Args, Command};
 use crate::cli::help;
+use crate::core::err::InitError;
+use crate::core::init::OpenInit;
+use crate::daemon::system::OpenDaemon;
+use crate::sys::sysrq;
 
 use crate::colors::{BOLD, C_RESET, RED};
 
 pub const CLEANUP_SCREEN: bool = false;
 
+mod cli;
 mod colors;
 mod core;
 mod daemon;
-mod util;
 mod sys;
-mod cli;
+mod util;
 
 fn main() {
     let args = Args::collect();
@@ -26,12 +26,8 @@ fn main() {
 
     match args.command {
         Command::Help => help::help(),
-        Command::Reboot => {
-            sysrq::sys_reboot();
-        },
-        Command::Shutdown => {
-            sysrq::sys_shutdown();
-        },
+        Command::Reboot => sysrq::sys_reboot(),
+        Command::Shutdown => sysrq::sys_shutdown(),
         Command::Panic => println!("Not implemented"),
         _ => {
             openinit.start(opendaemon).unwrap_or_else(|err| {
